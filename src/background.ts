31d33745b97d125a28a471d1218dc68887279032
sci-barite <romo.mulas@gmail.com>
2022-10-05 00:05:29 +0300

    Sylph - 1.5.8 Dao

--- src/background.ts
+++ src/background.ts
@@ -1,19 +1,33 @@
+var Tab : number;
 var CastingIndex = 1;
 var SylphCasting = false;
-var Tab : number;
 
 function SylphCasts()
 {               
    if ( SylphCasting )
    {
-      chrome.pageAction.setIcon({tabId: Tab, path: 'images/sylph-casts'+CastingIndex+'.png'});
+      chrome.action.setIcon({tabId: Tab, path: 'images/sylph-casts'+CastingIndex+'.png'});
       CastingIndex = (CastingIndex + 1) % 10;
-      window.setTimeout(SylphCasts, 160); // Sylph spell-casting animation for the win!!
+      setTimeout(() => SylphCasts(), 150); // Sylph spell-casting animation for the win!!
    }
 }
 
 chrome.runtime.onInstalled.addListener(()=> {
     console.log('Sylph awaits your orders!');
+    chrome.action.disable();
+
+    let rules = {
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({pageUrl: { hostSuffix: '.linkedin.com', pathPrefix: '/in', schemes: ['https'] }}),
+          new chrome.declarativeContent.PageStateMatcher({pageUrl: { hostSuffix: 'djinni.co', pathPrefix: '/home/inbox', schemes: ['https'] }}),
+          new chrome.declarativeContent.PageStateMatcher({pageUrl: { hostSuffix: '.upwork.com', pathPrefix: '/ab/applicants', schemes: ['https'] }}),
+          new chrome.declarativeContent.PageStateMatcher({pageUrl: { hostSuffix: '.upwork.com', pathPrefix: '/freelancers', schemes: ['https'] }}),
+        ],
+        actions: [ new chrome.declarativeContent.ShowAction() ]
+      };
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+        chrome.declarativeContent.onPageChanged.addRules([rules]);
+      });
 });
 
 chrome.bookmarks.onCreated.addListener((id, bookmark)=> {
@@ -24,11 +38,11 @@ chrome.bookmarks.onCreated.addListener((id, bookmark)=> {
         url.includes("djinni.co/home")) {
         chrome.bookmarks.get((bookmark.parentId as string), (folder) => {
             chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-                Tab = (tabs[0].id as number)
-                chrome.tabs.sendMessage(Tab, { name: 'Sylph', site: url, position: folder[0].title });
-                console.log("Bookmark created in '"+folder[0].title+"', Sylph is casting her spell...");
+                Tab = (tabs[0].id as number);
                 SylphCasting = true;
                 SylphCasts(); // Starts the animation of the icon!
+                chrome.tabs.sendMessage(Tab, { name: 'Sylph', site: url, position: folder[0].title });
+                console.log("Bookmark created in '"+folder[0].title+"', Sylph is casting her spell...");
             });
         });
     }
@@ -37,11 +51,11 @@ chrome.bookmarks.onCreated.addListener((id, bookmark)=> {
 chrome.runtime.onMessage.addListener(function(Sylph) {
     if (Sylph.SpellSuccessful) {
         SylphCasting = false;
-        chrome.pageAction.setIcon({tabId: Tab, path: "images/sylph32.png"}); // Stops animation, puts default icon.
+        chrome.action.setIcon({tabId: Tab, path: "images/sylph32.png"}); // Stops animation, puts default icon.
         console.log("Sylph has casted her spell successfully!");
     }
     else {
         SylphCasting = false;
-        chrome.pageAction.setIcon({tabId: Tab, path: "images/sylph-hurt.png"}); // Stops animation, puts hurt icon.
+        chrome.action.setIcon({tabId: Tab, path: "images/sylph-hurt.png"}); // Stops animation, puts hurt icon.
     }
 });
