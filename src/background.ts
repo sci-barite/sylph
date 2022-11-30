d98173e907abdb0f871006654b44522cebf7a80d
sci-barite <romo.mulas@gmail.com>
2022-11-30 13:59:57 +0200

    Sylph - 1.6.2 Dytto (Refactored globals in background, now using const objects, not sure how much better it is)

--- src/background.ts
+++ src/background.ts
@@ -1,14 +1,11 @@
-var Tab : number;
-var CastingIndex = 1;
-var SylphCasting = false;
-// Should find a way to get rid of these globals. Maybe encapsulating SylphCasts wihin a wrapper function, with recursion only within it.
-var ExistingID = '';
+const SylphState = {Casting : false, Tab : 999, Spell : 1};
+const LancerState = {ExistingID : '', UniqueIDs: ['']};
+// I wonder if this is a better way to handle things than using vars, or the same thing.
 
-function SylphCasts(speed : number)
-{               
-    if ( SylphCasting ) {
-        chrome.action.setIcon({tabId: Tab, path: 'images/sylph-casts'+CastingIndex+'.png'});
-        CastingIndex = (CastingIndex + 1) % 10;
+function SylphCasts(speed : number) {               
+    if (SylphState.Casting) {
+        chrome.action.setIcon({tabId: SylphState.Tab, path: 'images/sylph-casts'+SylphState.Spell+'.png'});
+        SylphState.Spell = (SylphState.Spell + 1) % 10;
         setTimeout(() => SylphCasts(speed), speed); // Sylph spell-casting animation for the win!!
     }
 }
@@ -16,7 +13,7 @@ function SylphCasts(speed : number)
 // If we don't do this, it will continue to try to animate the icon in that tab forever. How to manage this without globals?
 // Do we need a new function just to check if a tab is "casting" or not?
 chrome.tabs.onRemoved.addListener(tabID => {
-    if (tabID == Tab && SylphCasting == true) SylphCasting = false;
+    if (tabID == SylphState.Tab && SylphState.Casting == true) SylphState.Casting = false;
 })
 
 // This is not very useful, because it doesn't allow for changes in the title, only in the icon and only through canvas.
@@ -41,15 +38,15 @@ chrome.runtime.onInstalled.addListener(()=> {
 
 // This is the main way the extension works: when a bookmark is created, we send a message to the content script, which will process the page.
 chrome.bookmarks.onCreated.addListener((id, bookmark)=> {
-    var url = bookmark.url as string;
+    const url = bookmark.url as string;
     if (url.includes("in.com/in") || url.includes("in.com/jobs/view") || url.includes('o.io/?utm') || // We're into the whole brevity thing.
         url.includes("rk.com/ab/applicants") || url.includes("rk.com/free") || url.includes("nni.co/home/inbox") || url.includes('o.io/#')) {
         chrome.bookmarks.get((bookmark.parentId!), folder => {   // chrome.bookmarks.get is async: we need to act in its callback.
             chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-                Tab = (tabs[0].id as number);
-                SylphCasting = true;
+                SylphState.Tab = (tabs[0].id as number);
+                SylphState.Casting = true;
                 SylphCasts(150); // Starts the animation of the icon!
-                chrome.tabs.sendMessage(Tab, { name: 'Sylph', site: url, ex: ExistingID, position: folder[0].title });
+                chrome.tabs.sendMessage(SylphState.Tab, { name: 'Sylph', site: url, ex: LancerState.ExistingID, position: folder[0].title });
                 console.log("🧚‍♀️ Bookmark created in '"+folder[0].title+"', Sylph is casting her spell...");
             });
         });
@@ -58,23 +55,23 @@ chrome.bookmarks.onCreated.addListener((id, bookmark)=> {
 
 chrome.runtime.onMessage.addListener(Sylph => {
     chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-        Tab = (tabs[0].id as number);
+        SylphState.Tab = (tabs[0].id as number);
         if (Sylph.SpellSuccessful) {    // Success!
-            SylphCasting = false;
-            chrome.action.setIcon({tabId: Tab, path: "images/sylph32.png"}); // Stops animation, puts default icon.
+            SylphState.Casting = false;
+            chrome.action.setIcon({tabId: SylphState.Tab, path: "images/sylph32.png"}); // Stops animation, puts default icon.
             console.log("🧚‍♀️ Sylph has casted her spell successfully!");
-            chrome.action.setTitle({tabId: Tab, 
+            chrome.action.setTitle({tabId: SylphState.Tab, 
                 title: "🧜‍♂️ Lancer's response was:\n\n"+Sylph.LancerResponse+'\n'});
         }
         else if (!Sylph.SpellSuccessful && !Sylph.Lancer) { // This is an error.
-            SylphCasting = false;
-            chrome.action.setIcon({tabId: Tab, path: "images/sylph-hurt.png"}); // Stops animation, puts hurt icon.
+            SylphState.Casting = false;
+            chrome.action.setIcon({tabId: SylphState.Tab, path: "images/sylph-hurt.png"}); // Stops animation, puts hurt icon.
             console.log("🧚‍♀️ Sylph has miscasted!");
-            chrome.action.setTitle({tabId: Tab, 
+            chrome.action.setTitle({tabId: SylphState.Tab, 
                 title: "🧚‍♀️ Sylph has miscasted!\n🧜‍♂️ Lancer's response was:\n\n"+Sylph.LancerResponse+'\n'});
         }
         else if (Sylph.Lancer) {    // This happens when we load a job page: Lancer sends us uniqueIDs, so we know what entry to update.
-            SylphCasting = true;
+            SylphState.Casting = true;
             SylphCasts(60);
             console.log('🧚‍♀️ Sylph is summoning Lancer...');
             fetch(
@@ -83,29 +80,28 @@ chrome.runtime.onMessage.addListener(Sylph => {
             )
             .then((response) => response.text())
             .then((data) => {
-                let UniqueJobs = data;
-                let JobURL = Sylph.Place;
-                let JobID = JobURL.split("view/")[1];
-                let JobsArray = UniqueJobs.split(',');
-                let JobIndex;
-                JobsArray.indexOf(JobID!.split('/')[0]) ? 
-                    JobIndex = JobsArray.indexOf(JobID!.split('/')[0]) : JobIndex = JobsArray.indexOf(JobID);
+                LancerState.UniqueIDs = data.split(',');    // It overwrites the data everytime, to react to indexing changes on the sheet. Needed?
+                const UniqueIDs = LancerState.UniqueIDs.length
+                const JobURL = Sylph.Place;
+                const JobID = JobURL.split("view/")[1];
+                const JobIndex = LancerState.UniqueIDs.indexOf(JobID!.split('/')[0]) ? 
+                    LancerState.UniqueIDs.indexOf(JobID!.split('/')[0]) : LancerState.UniqueIDs.indexOf(JobID);
                 if (JobIndex != -1) {
-                    ExistingID = JobIndex.toString();
-                    SylphCasting = false; 
-                    chrome.action.setIcon({tabId: Tab, path: "images/sylph-hurt.png"});
-                    console.log("🧜‍♂️ Lancer knows this place! He wrote it as "+JobID!.split('/')[0]+' in row '+(parseInt(ExistingID)+2));
-                    chrome.action.setTitle({tabId: Tab, 
-                        title: "🧜‍♂️ Lancer knows this place!\nHe wrote it as "+JobID!.split('/')[0]+' in row '+(parseInt(ExistingID)+2)+'\n'
+                    LancerState.ExistingID = JobIndex.toString();
+                    SylphState.Casting = false; 
+                    chrome.action.setIcon({tabId: SylphState.Tab, path: "images/sylph-hurt.png"});
+                    console.log("🧜‍♂️ Lancer knows this place! He wrote it as "+JobID!.split('/')[0]+' in row '+(parseInt(LancerState.ExistingID)+2));
+                    chrome.action.setTitle({tabId: SylphState.Tab, 
+                        title: "🧜‍♂️ Lancer knows this place!\nHe wrote it as "+JobID!.split('/')[0]+' in row '+(parseInt(LancerState.ExistingID)+2)+'\n'
                                 +"Click on the ⭐ to update it.\n"})
                 }
                 else {
-                    ExistingID = '';
-                    SylphCasting = false;
-                    chrome.action.setIcon({tabId: Tab, path: "images/sylph32.png"});
-                    console.log("🧜‍♂️ Lancer doesn't know this place. The last he wrote was "+UniqueJobs.substring(UniqueJobs.length - 10));
-                    chrome.action.setTitle({tabId: Tab, 
-                        title: "🧜‍♂️ Lancer doesn't know this place.\nThe last he wrote was "+UniqueJobs.substring(UniqueJobs.length - 10)
+                    LancerState.ExistingID = '';
+                    SylphState.Casting = false;
+                    chrome.action.setIcon({tabId: SylphState.Tab, path: "images/sylph32.png"});
+                    console.log("🧜‍♂️ Lancer doesn't know this place. The last he wrote was "+LancerState.UniqueIDs[UniqueIDs - 1]);
+                    chrome.action.setTitle({tabId: SylphState.Tab, 
+                        title: "🧜‍♂️ Lancer doesn't know this place.\nThe last he wrote was "+LancerState.UniqueIDs[UniqueIDs - 1]
                                 +'\n'+"Click on the ⭐ to add this!\n"})
                 }
             });
