27f7b4ad9075cf60799dc790a0d2a26badfd628e
sci-barite <romo.mulas@gmail.com>
2022-12-02 18:35:14 +0200

    Sylph - 1.6.3 Dytto (Renaming of messages and their props)

--- src/background.ts
+++ src/background.ts
@@ -48,7 +48,7 @@ chrome.bookmarks.onCreated.addListener((id, bookmark)=> {
                 SylphAnimation[tabID] = 1; // Setup the animation for this tab only!
                 SylphCasts(tabID, 150); // Starts the animation of the icon!
                 const knownID = (LancerNumbers[tabID]) ? LancerNumbers[tabID] : '';
-                chrome.tabs.sendMessage(tabID, { '🧚‍♀️': 'SiftSpell', '🗂': tabID, '🌍': url, '💌': knownID, '📁': folder[0].title });
+                chrome.tabs.sendMessage(tabID, { '🧚‍♀️': 'SiftSpell', '🗃️': tabID, '🌍': url, '💌': knownID, '📁': folder[0].title });
                 console.log('🧚‍♀️ Bookmark created in "'+folder[0].title+'", Sylph is casting her spell from '+tabID+'...');
             });
         });
@@ -56,21 +56,21 @@ chrome.bookmarks.onCreated.addListener((id, bookmark)=> {
 });
 
 // This reacts to the content script's actions; themselves triggered either by this background script's messages, or by the onLoad event.
-chrome.runtime.onMessage.addListener(Sylph => {
-    switch(Sylph['🧚‍♀️']) {
+chrome.runtime.onMessage.addListener(Msg => {
+    switch(Msg['🧚‍♀️']) {
         case 'SpellSuccessful':    // Success!
-            delete SylphAnimation[Sylph['🗂']];  // This stops the animation!
-            chrome.action.setIcon({tabId: Sylph['🗂'], path: "images/sylph32.png"}); // Change back to default icon.
+            delete SylphAnimation[Msg['🗃️']];  // This stops the animation!
+            chrome.action.setIcon({tabId: Msg['🗃️'], path: "images/sylph32.png"}); // Change back to default icon.
             console.log("🧚‍♀️ Sylph has casted her spell successfully!");
-            chrome.action.setTitle({tabId: Sylph['🗂'], title: "🧜‍♂️ Lancer's response was:\n\n"+Sylph['🧜‍♂️']+'\n'});
+            chrome.action.setTitle({tabId: Msg['🗃️'], title: "🧜‍♂️ Lancer's response was:\n\n"+Msg['🧜‍♂️']+'\n'});
             break;
         case 'SpellFailed': // This is an error.
-            delete SylphAnimation[Sylph['🗂']];  // This stops the animation!
-            chrome.action.setIcon({tabId: Sylph['🗂'], path: "images/sylph-hurt.png"}); // Stops animation, puts hurt icon.
+            delete SylphAnimation[Msg['🗃️']];  // This stops the animation!
+            chrome.action.setIcon({tabId: Msg['🗃️'], path: "images/sylph-hurt.png"}); // Stops animation, puts hurt icon.
             console.log("🧚‍♀️ Sylph has miscasted!");
-            if (Sylph['🧜‍♂️'])
-                chrome.action.setTitle({tabId: Sylph['🗂'], title: "🧚‍♀️ Sylph has miscasted!\n🧜‍♂️ Lancer's response was:\n\n"+Sylph['🧜‍♂️']+'\n'});
-            else chrome.action.setTitle({tabId: Sylph['🗂'], title: "🧚‍♀️ Sylph has miscasted!\nLancer could not be summoned!\n"});
+            if (Msg['🧜‍♂️'])
+                chrome.action.setTitle({tabId: Msg['🗃️'], title: "🧚‍♀️ Sylph has miscasted!\n🧜‍♂️ Lancer's response was:\n\n"+Msg['🧜‍♂️']+'\n'});
+            else chrome.action.setTitle({tabId: Msg['🗃️'], title: "🧚‍♀️ Sylph has miscasted!\nLancer could not be summoned!\n"});
             break;
         case 'LancerSummon':   // This happens when we load a job page: Lancer sends us uniqueIDs, so we know what entry to update.
             chrome.tabs.query({ active: true, currentWindow: true }, tabs => {  // This time we need to find the tab: content scripts can't.
@@ -78,9 +78,9 @@ chrome.runtime.onMessage.addListener(Sylph => {
                 SylphAnimation[tabID] = 1; // Setup the animation for this tab only!
                 SylphCasts(tabID, 60);  // Starts the animation of the icon!
                 console.log('🧚‍♀️ Sylph is summoning 🧜‍♂️ Lancer...');
-                fetch(Sylph['🧜‍♂️']+'url=GetUniqueJobs').then((response) => response.text()).then((data) => {
+                fetch(Msg['🧜‍♂️']+'url=GetUniqueJobs').then((response) => response.text()).then((data) => {
                     const LancerIDs = data.split(',');    // Might be better to cache this in localStorage, but for now I want live changes.
-                    const JobID = Sylph['🌍'].split("view/")[1].replace('/', '');
+                    const JobID = Msg['🌍'].split("view/")[1].replace('/', '');
                     const JobIndex = LancerIDs.indexOf(JobID);
                     delete SylphAnimation[tabID];
                     if (JobIndex != -1) {
--- src/content.ts
+++ src/content.ts
@@ -3,28 +3,28 @@ window.onload = () => {
     if (document.URL.includes("in.com/jobs/view")) chrome.runtime.sendMessage({'🧚‍♀️': 'LancerSummon', '🧜‍♂️': LancerWebApp, '🌍': document.URL});
 }
 // All the work is done by this listener, and the functions it calls (from scripts that are injected selectively depending on the website.)
-chrome.runtime.onMessage.addListener(Sylph => {
-    if (Sylph['🧚‍♀️'] == 'SiftSpell') {
-        console.log('🧚‍♀️ Sylph Sifts!', Sylph);
+chrome.runtime.onMessage.addListener(Msg => {
+    if (Msg['🧚‍♀️'] == 'SiftSpell') {
+        console.log('🧚‍♀️ Sylph Sifts!', Msg);
         let SiftedParams : string;
-        switch (Sylph['🌍'].substring(12,18)) {
-            case "linked": SiftedParams = SiftLinked(Sylph['🗂'], Sylph['🌍']); break; // The function checks if it's a profile or job.
-            case "ni.co/": SiftedParams = SiftDjinni(Sylph['🗂']); break;              // This one uses the folder only on one condition.
-            case "apollo": SiftedParams = SiftApollo(Sylph['🌍']); break;              // This needs the URL just to build a better link.
-            case "upwork": SiftedParams = SiftUpwork(Sylph['🗂'], Sylph['🌍']); break; // The function checks if it's a profile or proposal.
-            default: alert(Sylph.Place.substring(12,18)+": This portion of the URL is not recognized!"); return;
+        switch (Msg['🌍'].substring(12,18)) {
+            case "linked": SiftedParams = SiftLinked(Msg['📁'], Msg['🌍']); break; // The function checks if it's a profile or job.
+            case "ni.co/": SiftedParams = SiftDjinni(Msg['📁']); break;              // This one uses the folder only on one condition.
+            case "apollo": SiftedParams = SiftApollo(Msg['🌍']); break;              // This needs the URL just to build a better link.
+            case "upwork": SiftedParams = SiftUpwork(Msg['📁'], Msg['🌍']); break; // The function checks if it's a profile or proposal.
+            default: alert(Msg['🌍'].substring(12,18)+": This portion of the URL is not recognized!"); return;
         }
-        const LancerURI = LancerWebApp + SiftedParams + '&ex='+Sylph['💌'];
-        if (Sylph['💌']) console.log('🧜‍♂️ Lancer has a record of this at '+(parseInt(Sylph['💌'])+2)+'!');
+        const LancerURI = LancerWebApp + SiftedParams + '&ex='+Msg['💌'];
+        if (Msg['💌']) console.log('🧜‍♂️ Lancer has a record of this at '+(parseInt(Msg['💌'])+2)+'!');
         console.log('🧚‍♀️ -> 🧜‍♂️\n'+LancerURI);
         const Lancer = new XMLHttpRequest();
         Lancer.onreadystatechange = () => {
             if (Lancer.readyState === XMLHttpRequest.DONE) {
                 console.log(Lancer.response);
-                if (Lancer.status === 200) chrome.runtime.sendMessage({'🧚‍♀️': 'SpellSuccessful', '🧜‍♂️': Lancer.response, '🗂': Sylph['🗂']});
+                if (Lancer.status === 200) chrome.runtime.sendMessage({'🧚‍♀️': 'SpellSuccessful', '🧜‍♂️': Lancer.response, '🗃️': Msg['🗃️']});
                 else if (Lancer.status === 0 || (Lancer.status > 200 && Lancer.status < 400)) 
-                    chrome.runtime.sendMessage({'🧚‍♀️': 'SpellFailed', '🧜‍♂️': Lancer.response, '🗂': Sylph['🗂']});   
-                else chrome.runtime.sendMessage({'🧚‍♀️': 'SpellFailed', '🗂': Sylph['🗂']}); // In this case, Lancer must not have been reached.
+                    chrome.runtime.sendMessage({'🧚‍♀️': 'SpellFailed', '🧜‍♂️': Lancer.response, '🗃️': Msg['🗃️']});   
+                else chrome.runtime.sendMessage({'🧚‍♀️': 'SpellFailed', '🗃️': Msg['🗃️']}); // In this case, Lancer must not have been reached.
             }
         }
         Lancer.open('GET', LancerURI, true);
