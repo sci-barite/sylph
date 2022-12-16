369766a53282e9618b9de9017171e97986b8220f
sci-barite <romo.mulas@gmail.com>
2022-12-16 23:58:13 +0200

    Sylph 1.6.4 Dytto (Comment)

--- src/content.ts
+++ src/content.ts
@@ -1,7 +1,5 @@
-// This is to check for existing entries of the job. The work is done by the service worker, not to slow down the page itself.
-window.onload = () => {
-    if (document.URL.includes("in.com/jobs/view")) chrome.runtime.sendMessage({'🧜‍♂️': LancerWebApp, '🌍': document.URL});
-}
+// This is to check for existing entries. The work is done by the service worker, not to slow down the page itself.
+window.onload = () => {chrome.runtime.sendMessage({'🧜‍♂️': LancerWebApp, '🌍': document.URL})}
 
 // All the work is done by this listener, and the functions it calls (from scripts that are injected selectively depending on the website.)
 chrome.runtime.onMessage.addListener(Msg => {
