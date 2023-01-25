9e4746bd9fd9f41a4a2250750187967ed4f68cf6
sci-barite <romo.mulas@gmail.com>
2023-01-25 13:50:10 +0200

    Sylph 1.6.6 Dytto (Experimental refactor going forward)

--- src/background.ts
+++ src/background.ts
@@ -110,7 +110,11 @@ function checkID(data: string | string[], Msg: {[key: string]: any}) {
 
 // MESSAGE LISTENER: Reacts to the content script's actions; themselves replies to either this service worker's messages, or the onLoad event.
 chrome.runtime.onMessage.addListener(async Msg => {
-    if      (Msg['📃']) fetch(Msg['🧜‍♂️'], {method: 'POST', body: 'ApolloList:'+(Msg['📃'])}).then(() => Shout(Msg, `🧚‍♀️ Sylph lists!\n`));
+    if (Msg['📃']) fetch(Msg['🧜‍♂️'], {method: 'POST', body: 'ApolloList:'+(Msg['📃'])}).then(response => response.text()).then(data => {
+        const Row = data.split(':')[0].slice(-4)
+        data.includes('🧜‍♂️') ? (Msg['✔️'] = data, Msg['📝'] = Number.isNaN(parseInt(Row)) ? Row.split(' ')[1] : Row) : Msg['❌'] = data;
+         if (Msg['✔️']) Shout(Msg, `🧚‍♀️ Sylph has casted her spell successfully!\n`, `\n🧜‍♂️ Lancer's response was:\n\n${Msg['✔️']}\n`);
+         else Shout(Msg, `🧚‍♀️ Sylph has casted her spell successfully, but Lancer did't!\n`, `\n🧜‍♂️ His response was:\n\n${Msg['❌']}\n`)});
     else if (Msg['✔️']) Shout(Msg, `🧚‍♀️ Sylph has casted her spell successfully!\n`, `\n🧜‍♂️ Lancer's response was:\n\n${Msg['✔️']}\n`);
     else if (Msg['❓']) Shout(Msg, `🧚‍♀️ Sylph has lost Lancer!\n🧜‍♂️ He's left a clue:\n\n${Msg['❓']}\n`);
     else if (Msg['❌']) Shout(Msg, `🧚‍♀️ Sylph has miscasted!\n\n${Msg['❌']}\n`);
--- src/sift-apollo.ts
+++ src/sift-apollo.ts
@@ -5,14 +5,14 @@ type ApolloContact = {
 
 // The only tricky thing here is it uses the same parameters for different things. Now that we return the params, we could make this better.
 function apolloSift(Msg: {[key: string]: any}) : {Failed:boolean, String:string} {
-    const apolloContactSetup  : {[key: string]: string}  = {
+    const apolloContactSetup = {    // Conditions, error messages, alternative parsing, and data to format the URL
         condition1: '#location_detail_card', condition1a: 'tr.zp_cWbgJ', condition2: '.zp_I1ps2',
-        error1: '❌ She sees no human!', error2: '❌ She sees no account!', alternativeSift: 'apolloListSift',
-        outputURL1: 'apollo/contacts/', outputURL2: 'apollo/people/', uniqueIDstringLength: '24'
+        error1: '❌ She sees no human here!', error2: "❌ This human's account has been exiled!", alternativeSift: 'apolloListSift'
     }
-    const apolloClasses = { signalsPanelEntries: '.zp_KqZzF', company_links: '.zp_I1ps2', allLinks: 'a.zp-link.zp_OotKe',
+    const apolloClasses = { 
+        signalsPanelEntries: '.zp_KqZzF', company_links: '.zp_I1ps2', allLinks: 'a.zp-link.zp_OotKe',
         email: "a.zp-link.zp_OotKe.zp_dAPkM.zp_Iu6Pf", company: '.zp_lMvaV.zp_JKOw3', phone: 'a.zp-link.zp_3_fnL.zp_1AaQP',
-        title: '.zp_LkFHT', location: '.zp_hYCdb.zp_CZF_z', employees: '.zp_nowuD.zp_a_Jcv', name: '.zp__iJHP', 
+        title: '.zp_LkFHT', location: '.zp_hYCdb.zp_CZF_z', employees: '.zp_nowuD.zp_a_Jcv', name: '.zp__iJHP'
     }
     const positions = ['ngineer', 'eveloper', 'esigner', 'ester', 'rogrammer'];
     const elements : {[key: string]: NodeListOf<HTMLElement>} = {};
@@ -20,27 +20,22 @@ function apolloSift(Msg: {[key: string]: any}) : {Failed:boolean, String:string}
     Object.entries(apolloContactSetup).filter(tuple => tuple[0].startsWith('condition')).concat(Object.entries(apolloClasses))
     .forEach(tuple => (elements[tuple[0]] = document.querySelectorAll(tuple[1])));  // Not to have to do this manually for all.
 
-    if (!elements.condition1[0]) {
-        if (!elements.condition1a[0])   // Shows there is a table of (hopefully) contacts.
-            return {Failed: true, String: apolloContactSetup.error1};
-        else return window[apolloContactSetup.alternativeSift](apolloContactSetup.condition1a[0]);   // Preparing for something big.
+    if (!elements.condition1[0]) {  // Check if it's not a page with a contact's profile
+        if (!elements.condition1a[0]) return {Failed: true, String: apolloContactSetup.error1};   // If no table either, page is not supported.
+        else return window[apolloContactSetup.alternativeSift](apolloContactSetup.condition1a);   // This calls the table parser function.
     }
-    if (!elements.condition2[0])
-        return {Failed: true, String: apolloContactSetup.error2};
+    if (!elements.condition2[0]) return {Failed: true, String: apolloContactSetup.error2};   // Means there is no company info associated.
 
     Array.from(elements.signalsPanelEntries).forEach(signal => { if (signal.innerText.includes('Job')) signal!.click()});   // Displays jobs
-
-    const URL = (document.URL.includes(apolloContactSetup.outputURL1) ? apolloContactSetup.outputURL1
-         : apolloContactSetup.outputURL2)+document.URL.substring(document.URL.length -parseInt(apolloContactSetup.uniqueIDstringLength));
     
     const company_link = elements.company_links[0].childNodes[0] as HTMLElement;
     const allLinks : HTMLElement[] = Array.from(document.querySelectorAll(apolloClasses.allLinks));
-    const linkedIn = allLinks.filter(ahref => ahref.attributes[1].value.includes("linkedin"));
+    const linkedIn = allLinks.filter(ahref => {if (ahref.attributes[1]) return ahref.attributes[1].value.includes("linkedin")});
     const jobs = allLinks.filter(elem => positions.some(position => elem.innerText.includes(position)));
 
-    const Contact : ApolloContact = {
+    const Sifted : ApolloContact = {
         Name: elements.name[0].innerText, 
-        Name_apollo: URL, 
+        Name_apollo: document.URL.includes('?') ? document.URL.split('?')[0] + document.URL.split('=cio')[1] : document.URL, 
         Name_linkedin: linkedIn[0] ? linkedIn[0].attributes[1].value : 'NA',
         Location: elements.location[0] ? elements.location[0].innerText : 'NA', 
         Jobs: jobs.length,
@@ -53,27 +48,27 @@ function apolloSift(Msg: {[key: string]: any}) : {Failed:boolean, String:string}
         Phone: elements.phone[0] ? elements.phone[0].innerText : '',
         Email: elements.email[0] ? elements.email[0].innerText : ''
     }
-    return {Failed: true, String: JSON.stringify([Contact])};  
+    return {Failed: true, String: JSON.stringify([Sifted])};  
 }
 
 function apolloListSift(rowIdent: string) : {Failed:boolean, String:string} {
-    const Table : {[key: string]: string}[] = [];
+    const Sifted : {[key: string]: string}[] = [];
     const Header = Array.from(document.querySelectorAll("th"))?.map(th => (th as HTMLElement).innerText);
     if (!Header.includes('Title')) return {Failed: true, String: '❌ She sees no humans in this list!'};
     const Columns = Header.map(column => column.includes(' ') ? column.split(' ')[1] : column);
     
     document.querySelectorAll(rowIdent).forEach((tr, Row) => {
-        Table.push({}); // An empty row is pushed first, to have an index to refer to, via Row.
+        Sifted.push({}); // An empty row is pushed first, to have an index to refer to, via Row.
         tr.childNodes.forEach((td, col) => {
-            Table[Row][Columns[col]] = (td as HTMLElement).innerText;
+            Sifted[Row][Columns[col]] = (td as HTMLElement).innerText;
             if (Columns[col] != 'Name' && Columns[col] != 'Company') return;    // No point in checking anything other than these two.
             const hrefs = (td as HTMLElement).innerHTML.match(/href="(.+?)"/g)?.map(str => str.split("\"")[1]) ?? [];
             hrefs.forEach(link => {
-                if (link.includes('#')) Table[Row][Columns[col]+'_apollo'] = link;
-                else if (link.includes('linkedin')) Table[Row][Columns[col]+'_linkedin'] = link;
-                else if (!link.includes('facebook') && !link.includes('twitter')) Table[Row][Columns[col]+'_web'] = link;
+                if (link.includes('#')) Sifted[Row][Columns[col]+'_apollo'] = link;
+                else if (link.includes('linkedin')) Sifted[Row][Columns[col]+'_linkedin'] = link;
+                else if (!link.includes('facebook') && !link.includes('twitter')) Sifted[Row][Columns[col]+'_web'] = link;
             });
         });
     });
-    return {Failed: true, String: JSON.stringify(Table)}
+    return {Failed: true, String: JSON.stringify(Sifted)}
 }
